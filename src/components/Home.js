21bc33e22de4a8ad0fa86b3a72d42dd4dfa3e2d7
SUSHANT0002 <sushant.mahadwad@gmail.com>
2023-10-24 17:27:28 +0530

    animation on home done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,42 +1,51 @@
 import React, { useEffect,useRef } from "react";
 import Navbar from "./Navbar";
-// import NET from 'vanta/src/vanta.net'
-import * as THREE from 'three';
+// import {NET} from 'vanta'
+import NET from 'vanta/src/vanta.net'
 const Home = () => {
-  const containerRef = useRef();
+  const vantaRef = useRef(null);
 
   useEffect(() => {
-    // Create a scene, camera, and renderer
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
+    const vantaEffect = NET({
+      el: vantaRef.current, 
+      mouseControls: true, 
+      touchControls: true, 
+      gyroControls: false,
+      minHeight: 700.00,
+      Width: 100.00,
+      top:100.00,
+      paddingTop: -100.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      color: 0xafff00,
+      backgroundColor: 0x00000,
+      points: 5.00,
+      maxDistance: 16.00,
+      spacing: 10.00,
+      paddingLeft:50.00
+      
+       });
 
-    // Set up your 3D objects, lights, and materials here
-
-    // Append the renderer to the component's container
-    containerRef.current.appendChild(renderer.domElement);
-
-    // Render loop
-    const animate = () => {
-      // Update the 3D scene here
-
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
+    return () => {
+      // Clean up the effect when the component unmounts
+      vantaEffect.destroy();
     };
-
-    animate();
   }, []);
+
+  
 
 
 
   return (
     <>
+    
       <Navbar />
-      <div style={{ paddingTop: "30px" }}>
-        <h1 style={{ textAlign: "center" }}>
+      <div className="parent">
+      <div className="child" style={{ paddingTop: "50px", paddingLeft:"30px"}}>
+        <h1 style={{ textAlign: "left" }}>
           <b>Welcome to TechChor: Empowering Student Innovators!</b>
         </h1>
-        <p style={{ fontSize: "22px" , paddingLeft:'40px',paddingTop:'20px',color:'white'}}>
+        <p style={{ fontSize: "22px" , paddingLeft:'0px',paddingTop:'20px',color:'white'}}>
           At TechChor, we believe in the power of innovation, collaboration, and
           knowledge sharing. We are more than just a student community; we are a
           vibrant ecosystem of tech enthusiasts, programmers, designers, and
@@ -44,6 +53,12 @@ const Home = () => {
           beginner with a passion for technology or an experienced coder looking
           to expand your horizons, TechChor is the perfect place for you.
         </p>
+        
+    
+    
+    </div>
+    <div ref={vantaRef} className="child1" >
+      </div>
       </div>
     </>
   );
